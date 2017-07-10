d0d3185d16ce27f15ffc211a9de50842f1bf0837
Stephen Houser <stephenhouser@gmail.com>
2017-07-09 21:59:21 -0400

    update and use volume for webwiz data

--- tools/create-data.sh
+++ tools/create-data.sh
@@ -3,11 +3,11 @@
 ## Copy WebWiz image files to webwiz volume
 echo "Copying data to webwiz volume..."
 docker run -v tdreplica_webwiz:/webwiz --name helper busybox true
-docker cp ./webwiz/ helper:/
+docker cp ../tdreplica.com/webwiz/ helper:/
 docker rm helper
 
 ## Copy user data/uploads to uploads volume
 echo "Copying data to uploads volume..."
 docker run -v tdreplica_uploads:/uploads --name helper busybox true
-docker cp ./uploads/ helper:/
+docker cp ../tdreplica.com/wp-content/uploads/ helper:/
 docker rm helper
